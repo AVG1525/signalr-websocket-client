a7ba35f935253562812554e6bd5bfbb0da69fb24
Aron Vinicius <aronvinicius15@gmail.com>
2022-09-09 12:29:27 -0300

    first commit

---
+++ README.md
@@ -0,0 +1,1 @@
+# signalr-websocket-client
---
+++ signalr-websocket-client/Program.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.AspNetCore.SignalR.Client;
+using System.Threading.Tasks;
+
+namespace signalr_websocket_client
+{
+    public class Program
+    {
+        static async Task Main()
+        {
+            var uri = "http://localhost:5000/server";
+
+            await using var connection = new HubConnectionBuilder().WithUrl(uri).Build();
+            await connection.StartAsync();
+
+            connection.On<dynamic>("Count", (message) => {
+                System.Console.WriteLine(message); 
+            });
+
+            System.Console.ReadLine();
+        }
+    }
+}
